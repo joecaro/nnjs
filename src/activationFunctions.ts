b2e840fe485ee1573019bae58159535ed5156f9a
joecaro <jsc11598@gmail.com>
2022-06-16 23:48:49 -0400

    Layer tests

---
+++ src/activationFunctions.ts
@@ -0,0 +1,15 @@
+import ActivationFunction from "./types/ActivationFunction";
+
+const activationFunctions: activationFunctionsType = {
+  relu: (value: number) => Math.max(0, value),
+  sigmoid: (value: number) => 1 / (1 + Math.pow(Math.E, -value)),
+  tanh: (value: number) => 2 / (1 + Math.pow(Math.E, -2 * value) + 1),
+};
+
+export type activationFunctionsType = {
+  relu: ActivationFunction;
+  sigmoid: ActivationFunction;
+  tanh: ActivationFunction;
+};
+
+export default activationFunctions;
---
+++ test/layer.test.ts
@@ -0,0 +1,43 @@
+import Layer from "../src/types/Layer";
+
+test("should init new layer with nodes and weights", () => {
+  let layer = new Layer(5, 5, "relu");
+
+  let expectedNodes = [
+    { value: 0, error: 0 },
+    { value: 0, error: 0 },
+    { value: 0, error: 0 },
+    { value: 0, error: 0 },
+    { value: 0, error: 0 },
+  ];
+  let expectedWeights = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+  ];
+
+  expect(layer.nodes).toMatchObject(expectedNodes);
+  expect(layer.weights).toMatchObject(expectedWeights);
+});
+
+test("should init with activation function", () => {
+  let layer = new Layer(5, 5, "relu");
+
+  expect(layer.activationFunction(1)).toBe(1);
+});
+
+test("should feed foward", () => {
+  let prevInputs = [{ value: 1, error: 0 }];
+  let layer = new Layer(1, prevInputs.length, "relu");
+
+  layer.weights = [[0.5]];
+  layer.feedForward(prevInputs);
+
+  expect(layer.nodes[0].value).toBe(0.5);
+});
+
+test("should back propogate", () => {
+  expect(true).toBeTruthy();
+});
