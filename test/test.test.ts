5e14cf7ea11ab06227c4d6239a2355343f318ebf
joecaro <jsc11598@gmail.com>
2022-06-16 23:49:08 -0400

    NN add layers & feed forward w/ tests

---
+++ test/test.test.ts
@@ -0,0 +1,43 @@
+import { NN } from "../src/index";
+
+test("NN inits with inputs/outpus", () => {
+  const nn = new NN(5, 5);
+  expect(nn.numberOfInputs).toBe(5);
+  expect(nn.numberOfOutputs).toBe(5);
+});
+test("NN adds first hidden layer", () => {
+  const nn = new NN(5, 5);
+
+  nn.addHiddenLayer(1, "relu");
+
+  expect(nn.hiddenLayers.length).toBe(1);
+});
+test("NN adds multiple hidden layers", () => {
+  const nn = new NN(5, 5);
+
+  nn.addHiddenLayer(3, "relu");
+  nn.addHiddenLayer(1, "relu");
+
+  expect(nn.hiddenLayers.length).toBe(2);
+});
+test("NN feeds forward", () => {
+  const nn = new NN(1, 1);
+
+  nn.addHiddenLayer(1, "relu");
+  nn.addHiddenLayer(1, "relu");
+
+  nn.hiddenLayers[0].weights = [[1]];
+  nn.hiddenLayers[1].weights = [[0.5]];
+  nn.outputLayer.weights = [[1]];
+  let inputs = [1];
+  nn.feedForward(inputs, [1]);
+
+  expect(nn.outputLayer.nodes[0].value).toBe(0.5);
+});
+// test("NN sets loss function", () => {
+//   const nn = new NN(5, 5);
+
+//   nn.addHiddenLayer(1, "relu");
+
+//   expect(nn.hiddenLayers.length).toBe(1);
+// });
