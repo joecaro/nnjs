8787f20f009e4610b2477d39469584a16ddeeb96
joecaro <jsc11598@gmail.com>
2022-06-19 10:52:22 -0400

    reworked tests for rewrite

--- test/matrix.test.ts
+++ test/matrix.test.ts
@@ -6,32 +6,108 @@ test("should init new matrix", () => {
   expect(matrix.matrix).toMatchObject([[0]]);
 });
 
-test("should create matrix from static method", () => {
-  let matrix = Matrix.fromArray([[0]]);
+//STATIC methods
+test("should create matrix from array - STATIC", () => {
+  let matrix = Matrix.fromArray([0]);
 
   expect(matrix.matrix).toMatchObject([[0]]);
 });
 
 test("should add two matrices from - STATIC", () => {
-  let newArr = Matrix.add([[1]], [[1]]);
+  let a = new Matrix(1, 1);
+  let b = new Matrix(1, 1);
+  a.matrix[0] = [1];
+  b.matrix[0] = [1];
 
-  expect(newArr).toMatchObject([[2]]);
-});
+  let matrix = Matrix.add(a, b);
 
-test("should divide matrix by number - STATIC", () => {
-  let newArr = Matrix.divNumber([[2]], 2);
-
-  expect(newArr).toMatchObject([[1]]);
-});
-
-test("should multiply matrix by number - STATIC", () => {
-  let newArr = Matrix.subtract([[2]], [[2]]);
-
-  expect(newArr).toMatchObject([[4]]);
+  expect(matrix.matrix).toMatchObject([[2]]);
 });
 
 test("should subtract number from matrix - STATIC", () => {
-  let newArr = Matrix.subtract([[2]], [[1]]);
+  let a = new Matrix(1, 1);
+  a.matrix[0] = [2];
+  let b = new Matrix(1, 1);
+  b.matrix[0] = [2];
+  let newMatrix = Matrix.subtract(a, b);
 
-  expect(newArr).toMatchObject([[1]]);
+  expect(newMatrix.matrix).toMatchObject([[0]]);
+});
+
+test("should multiply two matrices - STATIC", () => {
+  let a = new Matrix(1, 1);
+  a.matrix[0] = [2];
+  let b = new Matrix(1, 1);
+  b.matrix[0] = [2];
+  let newMatrix = Matrix.multiply(a, b);
+
+  expect(newMatrix.matrix).toMatchObject([[4]]);
+});
+
+test("should divide all matrix items by number - STATIC", () => {
+  let a = new Matrix(1, 1);
+  a.matrix[0] = [2];
+
+  let newMatrix = Matrix.divNumber(a, 2);
+
+  expect(newMatrix.matrix).toMatchObject([[1]]);
+});
+
+test("should multipy all matrix items by number - STATIC", () => {
+  let a = new Matrix(1, 1);
+  a.matrix[0] = [2];
+
+  let newMatrix = Matrix.multNumber(a, 2);
+
+  expect(newMatrix.matrix).toMatchObject([[4]]);
+});
+
+test("should perform fun on all items - STATIC", () => {
+  let a = new Matrix(1, 1);
+  a.matrix[0] = [2];
+
+  let newMatrix = Matrix.map(a, (val) => val * 2);
+
+  expect(newMatrix.matrix).toMatchObject([[4]]);
+});
+
+test("should transpose matrix - STATIC", () => {
+  let a = new Matrix(2, 1);
+  a.matrix[0] = [0];
+  a.matrix[1] = [1];
+
+  let newMatrix = Matrix.transpose(a);
+
+  expect(newMatrix.matrix).toMatchObject([[0, 1]]);
+});
+
+// other methods
+test("should multiply two matrices", () => {
+  let a = new Matrix(1, 1);
+  a.matrix[0] = [2];
+  let b = new Matrix(1, 1);
+  b.matrix[0] = [2];
+  a.multiply(b);
+
+  expect(a.matrix).toMatchObject([[4]]);
+});
+
+test("should add two matrices", () => {
+  let a = new Matrix(1, 1);
+  a.matrix[0] = [2];
+  let b = new Matrix(1, 1);
+  b.matrix[0] = [2];
+  a.add(b);
+
+  expect(a.matrix).toMatchObject([[4]]);
+});
+
+test("should perform math operation on all matrix items", () => {
+  let a = new Matrix(1, 1);
+  a.addNumber(1);
+  expect(a.matrix).toMatchObject([[1]]);
+  a.multNumber(2);
+  expect(a.matrix).toMatchObject([[2]]);
+  a.divNumber(2);
+  expect(a.matrix).toMatchObject([[1]]);
 });
--- test/test.test.ts
+++ test/test.test.ts
@@ -1,66 +1,7 @@
 import NN from "../src/index";
 
 test("NN inits with inputs/outpus", () => {
-  const nn = new NN(5, 5);
-  expect(nn.numberOfInputs).toBe(5);
-  expect(nn.numberOfOutputs).toBe(5);
-});
-
-test("NN adds first hidden layer", () => {
-  const nn = new NN(5, 5);
-
-  nn.addHiddenLayer(1, "relu");
-
-  expect(nn.hiddenLayers.length).toBe(1);
-});
-
-test("NN adds multiple hidden layers", () => {
-  const nn = new NN(5, 5);
-
-  nn.addHiddenLayer(3, "relu");
-  nn.addHiddenLayer(1, "relu");
-
-  expect(nn.hiddenLayers.length).toBe(2);
-});
-
-test("NN feeds forward", () => {
-  const nn = new NN(1, 1);
-
-  nn.outputLayer.weights = [[1]];
-  let inputs = [1];
-  nn.feedForward(inputs, [1]);
-
-  expect(nn.outputLayer.nodes[0].value).toBe(0.7310585786300049);
-  expect(nn.error).toBe(0.2689414213699951);
-});
-
-test("NN calculates node errors on feed forward", () => {
-  const nn = new NN(1, 1);
-
-  nn.outputLayer.weights = [[1]];
-  let inputs = [1];
-  nn.feedForward(inputs, [1]);
-
-  expect(nn.outputLayer.nodes[0].value).toBe(0.7310585786300049);
-  expect(nn.error).toBe(0.2689414213699951);
-});
-
-test("NN calculates loss", () => {
-  const nn = new NN(1, 1);
-
-  nn.calculateLoss([0], [1]);
-
-  expect(nn.error).toBe(1);
-});
-
-test("NN logs", () => {
-  const nn = new NN(5, 5);
-
-  nn.addHiddenLayer(5, "relu");
-
-  let message = nn.log();
-
-  expect(message).toBe(
-    `-----\nlayers:\n Inputs: 5\n Hidden Layer 1: 5\n Outputs: 5\n------`
-  );
+  const nn = new NN(2, 2, 2);
+  expect(nn.numberOfInputs).toBe(2);
+  expect(nn.numberOfOutputs).toBe(2);
 });
