a32a8b2622ee295aa76e4d3f498010f1ec9f3732
joecaro <jsc11598@gmail.com>
2022-06-19 12:47:49 -0400

    tests

--- test/layer.test.ts
+++ test/layer.test.ts
@@ -2,14 +2,14 @@ import Layer from "../src/classes/Layer/Layer";
 import Matrix from "../src/classes/Matrix/Matrix";
 
 test("should init new layer", () => {
-  let layer = new Layer(1, 1, "hidden");
+  let layer = new Layer(1, 1, "hidden", "sigmoid");
 
   expect(layer.weights.matrix).toMatchObject([[0]]);
   expect(layer.type).toBe("hidden");
 });
 
 test("should generate outputs", () => {
-  let layer = new Layer(1, 1, "hidden");
+  let layer = new Layer(1, 1, "hidden", "sigmoid");
   layer.weights.addNumber(1);
   layer.biases.addNumber(1);
 
--- test/test.test.ts
+++ test/test.test.ts
@@ -1,7 +1,17 @@
 import NN from "../src/index";
 
 test("NN inits with inputs/outpus", () => {
-  const nn = new NN(2, 2, 2);
+  const nn = new NN(2, 2);
   expect(nn.numberOfInputs).toBe(2);
   expect(nn.numberOfOutputs).toBe(2);
+});
+test("NN adds hidden layer", () => {
+  const nn = new NN(2, 2);
+
+  expect(nn.layers.length).toBe(1);
+
+  nn.addHiddenLayer(5);
+  expect(nn.layers.length).toBe(2);
+
+  expect(nn.outputLayer().numberOfInputs).toBe(5);
 });
